1a046631fab85549b98cee4f7d4857b913767538
Moses <moseslee9750@Gmail.com>
2023-02-05 00:34:22 -0800

    push

---
+++ health.js
@@ -0,0 +1,16 @@
+const foods = {
+  meat: [
+    "beef",
+    "chicken",
+    "pork",
+    "cheeseburger",
+    "hamburger",
+    "bacon",
+    "meat",
+  ],
+  vegetables: ["carrots", "broccoli", "potatoes", "lettuce", "kale", "spinach", "cauliflower", "asparagus", "cucumber", "onions", "garlic", "celery", "mushrooms", "zucchini", "corn"],
+  fruits: ["apples", "oranges", "bananas", "pineapple", "tomatoes", "lime", "avocado"],
+  dairy: ["milk", "cheese", "yogurt"],
+  grains: ["bread", "rice", "pasta", "macaroni", "risotto", "mashed potatoes", "brown rice", "quinoa", "beans", "black beans"],
+  snacks: ["chips", "candy", "cookies", "sugar", "chocolate", "cookie", "wafer", "waffle", "cake", "toffee", "pastry", "tiramisu"],
+};
